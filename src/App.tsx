c1dde66c769ae2890e8ce6d54ee5dd569ac3955a
soan4 <soanlabo@gmail.com>
2026-03-12 23:46:47 +0900

    fix unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { calcStock } from "./utils/calc";
 import type { ServiceLevel } from "./utils/calc";
 
